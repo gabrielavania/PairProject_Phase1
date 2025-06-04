199bfac9cf7d86131bf384ecaa9acfbfff058785
gabrielavania <gabriela.vania98@gmail.com>
2025-06-04 21:31:18 +0700

    migrate & seeding data

---
+++ seeders/20250604135809-categories.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const categories = require('../data/categories.json')
+    categories.forEach((el) => {
+      el.updatedAt = el.createdAt = new Date()
+    })
+
+    await queryInterface.bulkInsert('Categories', categories, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Categories', null, {
+      restartIdentity: true,
+      truncate: true
+    })
+  }
+};
