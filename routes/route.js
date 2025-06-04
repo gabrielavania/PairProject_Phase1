65b5a253b836362389d06aea6cf847714297bec1
Alfikri <alfzein19@gmail.com>
2025-06-04 23:35:10 +0700

    Feature: Able to login and logout, session is also fully implemented, dynamic login and logout

--- routes/route.js
+++ routes/route.js
@@ -5,16 +5,20 @@ const route = express.Router()
 //Landing Page
 route.get("/", Controller.showHome)
 //Login - Register
-
 route.get("/login", Controller.showLogin)
 route.post("/login", Controller.actualLogin)
 route.get("/register", Controller.showRegister)
 route.post("/register", Controller.actualRegister)
+route.get("/logout",Controller.logOut)
 
 //Profile Page
+// route.get("/:id/profile", Controller.X)
+// route.get("/:id/profile/edit", Controller.X)
+// route.post("/:id/profile/edit", Controller.X)
 
 
-//Forum Stuff
+
+//Post
 
 
 
